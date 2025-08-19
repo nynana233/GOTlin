63d7bab68bb45555e36d3cae42c3ee315bc9f88b
nanayaw <nynana233@gmail.com>
2025-08-19 09:59:11 -0700

    Add ComposeAppCommonTest.kt with an example test case

---
+++ composeApp/src/commonTest/kotlin/com/cdt/gotlin/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.cdt.gotlin
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
